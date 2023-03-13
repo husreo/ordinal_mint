ef08f4ff4a0e6cdf1e485c66aca33a974c23e2ea
husreo <springers230@gmail.com>
2023-03-13 20:51:36 +0300

    first commit

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import * as React from 'react';
+
+const links = [
+  { href: '/', label: 'Route 1' },
+  { href: '/', label: 'Route 2' },
+];
+
+export default function Header() {
+  return (
+    <header className='sticky top-0 z-50 bg-white'>
+      <div className='layout flex h-14 items-center justify-between'>
+        <Link href='/' className='font-bold hover:text-gray-600'>
+          Home
+        </Link>
+        <nav>
+          <ul className='flex items-center justify-between space-x-4'>
+            {links.map(({ href, label }) => (
+              <li key={`${href}${label}`}>
+                <Link href={href} className='hover:text-gray-600'>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo />
+
+      <main>
+        <section className='bg-white'>
+          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'></div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
